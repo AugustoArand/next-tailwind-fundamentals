214f6c4ef203d8fc43569fc72cf30425b0bc16b2
Augusto Arandiba dos Santos Junior <ajunior@reconcavotecnologia.org.br>
2025-07-14 16:04:03 -0300

    Envio inicial dos arquivos

---
+++ src/app/2_tipografia/page.tsx
@@ -0,0 +1,12 @@
+export default function Tipografia () {
+    return (
+        <div>
+            <h1 className="font-sans italic"> Titulo </h1>
+            <h1 className="font-serif italic"> Titulo </h1>
+            <h1 className="font-bold"> Titulo </h1>
+            <h1> Titulo </h1>
+            <h1> Titulo </h1>
+            <h1> Titulo </h1>
+        </div>
+    )
+}
---
+++ src/app/3_cores_fundo/page.tsx
@@ -0,0 +1,7 @@
+export default function CorFundo () {
+    return (
+        <div>
+            <h1 className="text-green-600 font-bold font-serif"> Titulo </h1>
+        </div>
+    )
+}
